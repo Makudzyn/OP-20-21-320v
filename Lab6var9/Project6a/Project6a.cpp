0c551eb156434b3ba303ef8d616bd81e67d328d9
Maksim Kunchenko <72383565+Makudzyn@users.noreply.github.com>
2020-11-22 20:51:48 +0200

    lab 6 complete fully

---
+++ Lab6var9/Project6a/Project6a.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <ctime>
+
+int main(){
+    	
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+
+	int pcNum = 0, userNum = 0, attCount = 0;
+
+	pcNum = rand() % 100;
+	printf("%d\n", pcNum); //Проверка числа компьютера
+
+	while (pcNum != userNum) {
+
+		printf("Угадайте целое число от 0 до 100:   ");
+		scanf_s("%d", &userNum);
+		attCount++;
+
+		if (userNum > pcNum) {
+			printf("Ваше число больше того, которое загадал компьютер!");
+
+		}
+		if (userNum < pcNum) {
+			printf("Ваше число меньше того, которое загадал компьютер!");
+
+		}
+		if (userNum == pcNum) {
+			printf("\nПравильно!Вы угадали число %d с %d-ой попытки\n", pcNum, attCount);
+		}
+	}
+
+
+
+
+
+
+}
---
+++ Lab6var9/Project6b/Project6b.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <math.h>
+#include <ctime>
+#include <stdlib.h>
+
+int main() {
+
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+
+	short a, b, rightRes, userRes, attCount = 0, points = 0;
+	
+
+	while (attCount < 10) {
+
+		a = rand() % 7 + 4;
+		b = rand() % 8 + 3;
+
+		rightRes = a * b;
+		attCount++;
+
+		printf("Пример: %hi * %hi\n", a,b);
+		printf("Введите ответ:  ");
+		scanf_s("%hi", &userRes);
+
+		printf("\n");
+
+		if (rightRes == userRes) {
+			points += 10;
+		}
+	}
+	printf("Ваша оценка:  %hi", points);
+}
