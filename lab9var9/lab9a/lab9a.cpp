ae1227badacdd7a151898ed2864977ccc731a8e2
Maksim Kunchenko <72383565+Makudzyn@users.noreply.github.com>
2020-12-14 09:49:24 +0200

    9 лаба допы

--- lab9var9/lab9a/lab9a.cpp
+++ lab9var9/lab9a/lab9a.cpp
@@ -1,18 +1,70 @@
 ﻿#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctime>
+
+int const N = 7;
+int const M = 9;
+int mas[N][M],mas2[N][M];
 
 int main(void) {
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+	int const lim = 100;
+	printf("Исходный массив\n");
+	for (int i = 0; i < N; i++) {
+		for (int k = 0; k < M; k++) {
+			mas[i][k] = rand() % lim + 1;
+			mas2[i][k] = mas[i][k];
+			printf("%4d", mas[i][k]);
+		}
+		printf("\n");
+	}
 
-
-
-
-
-
-
-
-
-
-
-
+	int itmp = 0, ktmp = 0, keep = 0;
+	/*1*/
+	for (int i = 0; i < N; i++) { // Столбцы
+		int max = 0;
+		for (int k = 0; k < M; k++) {
+			keep = mas[i][0];
+			if (mas[i][k] >= max) {                      
+				max = mas[i][k];
+				itmp = i; ktmp = k;
+			}
+			if (k == M - 1) {
+				mas[i][0] = max;
+				mas[itmp][ktmp] = keep;
+			}
+		}
+	}
+	printf("\nИзмененный массив(1)\n");
+	for (int i = 0; i < N; i++) {
+		for (int k = 0; k < M; k++) {
+			printf("%4d", mas[i][k]);
+		}
+		printf("\n");
+	}
+	/*2*/
+	for (int k = 0; k < M; k++) {
+		int min = lim;
+		for (int i = 0; i < N; i++) { // Столбцы
+			keep = mas2[N - 1][k];
+			if (mas2[i][k] <= min){
+				min = mas2[i][k];
+				itmp = i; ktmp = k;
+			}
+			if (i == N - 1) {
+				mas2[N-1][k] = min;
+				mas2[itmp][ktmp] = keep;
+			}
+		}
+	}
+	printf("\nИзмененный массив(2)\n");
+	for (int i = 0; i < N; i++) {
+		for (int k = 0; k < M; k++) {
+			printf("%4d", mas2[i][k]);
+		}
+		printf("\n");
+	}
 }
+
--- lab9var9/lab9b/lab9b.cpp
+++ lab9var9/lab9b/lab9b.cpp
@@ -1,18 +1,62 @@
 ﻿#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctime>
 
 int main(void) {
+	/*srand(time(0));*/
+	setlocale(LC_ALL, "ru");
 
+	int const X = 30;
+	int	mas1[X][X], mas2[X][X], finalmas[X][X] = { 0 };
+	int a1, b1, a2, b2;
 
+	printf("Задайте размерность первой матрицы(1-30) >> \n");
+	printf("\nКол-во столбцов : "); scanf_s("%d", &a1);
+	printf("\nКол-во строк    : "); scanf_s("%d", &b1);
 
+	printf("\nЗадайте размерность второй матрицы >> \n");
+	printf("\nКол-во столбцов : "); scanf_s("%d", &a2);
+	printf("\nКол-во строк    : "); scanf_s("%d", &b2);
 
+	while (a1 != b2 || a1 == 0 || a2 == 0 || b1 == 0 || b2 == 0) {
+		printf("\nПроизведение такой матрицы посчитать невозможно.\nПопробуйте еще раз: \n");
+		printf("\nЗадайте размерность первой матрицы >> \n");
+		printf("\nКол-во столбцов : "); scanf_s("%d", &a1);
+		printf("\nКол-во строк    : "); scanf_s("%d", &b1);
 
-
-
-
-
-
-
-
+		printf("\nЗадайте размерность второй матрицы >> \n");
+		printf("\nКол-во столбцов : "); scanf_s("%d", &a2);
+		printf("\nКол-во строк    : "); scanf_s("%d", &b2);
+	}
+	printf("\nПервая матрица\n");
+	for (int i = 0; i < a1; i++) {
+		for (int j = 0; j < b1; j++) {
+			mas1[i][j] = rand() % 10 - 5;
+			printf("%3d", mas1[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nВторая матрица\n");
+	for (int i = 0; i < a2; i++) {
+		for (int j = 0; j < b2; j++) {
+			mas2[i][j] = rand() % 5 + 1;
+			printf("%3d", mas2[i][j]);
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < a1; i++){
+		for (int j = 0; j < b2; j++){
+			for (int k = 0; k < a2; k++){
+				finalmas[i][j] += mas1[i][k] * mas2[k][j];
+			}
+		}
+	}
+	printf("\nПроизведение матриц\n");
+	for (int i = 0; i < a1; i++) {
+		for (int j = 0; j < b2; j++) {
+			printf("%4d", finalmas[i][j]);
+		}
+		printf("\n");
+	}
 }
