d2f57d44fd1ab1d8e80f560b162cf25af4206d4b
Maksim Kunchenko <72383565+Makudzyn@users.noreply.github.com>
2020-12-15 20:05:39 +0200

    Лаб 4

---
+++ lab4var9/lab4var9(2).cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    unsigned int channelForm = 0;                   // формат команды канала
+    char o, c, d;                                  // код операции O и признаки цепочки С и D
+    unsigned char bytes;                          // количество байтов для передачи N
+
+    printf("Формат команды канала (16cc от 0 до 0xFFFF) >>"); scanf_s("%x", &channelForm);
+    
+    o = (channelForm >> 11) & 0x1F;
+    c = (channelForm >> 10) & 1;
+    d = (channelForm >> 9) & 1;
+    bytes = channelForm & 0xFF;
+
+    printf("Код операции >> %d\n", o);
+    printf("Признак цепочки команд >> %d\n", c);
+    printf("Признак цепочки данных >> %d\n", d);
+    printf("Количество байтов для передачи >> %d\n", bytes);
+}
---
+++ lab4var9/lab4var9.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    unsigned int channelForm = 0;                   // формат команды канала
+    char o, c, d;                                  // код операции O и признаки цепочки С и D
+    unsigned char bytes;                          // количество байтов для передачи N
+
+    printf("Программа упаковки\n");
+
+    printf("\nВведите код операции (0 - 31) >> ");                    scanf_s("%hhd", &o);
+    
+    printf("\nПризнак цепочки команд (0 - 1) >> ");                   scanf_s("%hhd", &c);
+    
+    printf("\nВведите признак цепочки данных (0 - 1) >> ");           scanf_s("%hhd", &d);
+    
+    printf("\nВведите количество байтов для передачи (0 - 255) >> "); scanf_s("%hhd", &bytes);
+    
+    
+    channelForm |= ((unsigned char)o & 0x1F) << 11;  //Упаковывание кода
+    channelForm |= ((unsigned char)c & 1) << 10;
+    channelForm |= ((unsigned char)d & 1) << 9;
+    channelForm |= bytes & 0xFF;
+
+    /* вывод результата */
+    printf("\nУпакованная команда канала: %4x \n", channelForm);
+
+}
